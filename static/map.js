376e66d1a1c3cc714a2b3ce2dcf964a7f216f7c2
tora2025 <s2245186@g.tohoku-gakuin.ac.jp>
2025-09-13 17:57:41 +0900

    Update map.js

--- static/map.js
+++ static/map.js
@@ -11,22 +11,28 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // 被害データ取得
 fetch('/data')
   .then(response => response.json())
-  .then(geojson => {
-    L.geoJSON(geojson, {
+  .then(data => {
+    L.geoJSON(data, {
       onEachFeature: function (feature, layer) {
-        const damage = feature.properties.damage || '不明';
-        layer.bindPopup(`<div class="damage-popup">被害情報：${damage}</div>`);
-      },
-      pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-          radius: 8,
-          fillColor: "#ff0000",
-          color: "#fff",
-          weight: 1,
-          opacity: 1,
-          fillOpacity: 0.8
-        });
+        const p = feature.properties;
+
+        // rescue_needed を人が読みやすい文字に変換
+        let rescueText = "不明";
+        if (p.rescue_needed === true) rescueText = "はい";
+        else if (p.rescue_needed === false) rescueText = "いいえ";
+
+        const popupHtml = `
+          <div style="min-width:200px">
+            <b>災害種別:</b> ${p.damage || "不明"}<br>
+            <b>健康状態:</b> ${p.health_status || "不明"}<br>
+            <b>救助要否:</b> ${rescueText}<br>
+            <b>人数:</b> ${p.people_count ?? "不明"}<br>
+            <b>年齢層:</b> ${p.age_group || "不明"}<br>
+            <b>コメント:</b> ${p.comment || "なし"}
+          </div>
+        `;
+        layer.bindPopup(popupHtml);
       }
     }).addTo(map);
   })
-  .catch(error => console.error('データ取得エラー:', error));
+  .catch(err => console.error('データ読み込みエラー:', err));
